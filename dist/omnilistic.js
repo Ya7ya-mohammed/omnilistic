b1a3ac514e57a30297b1143bce3186df957fd263
Ya7ya-mohammed <suber_wolf@icloud.com>
2026-04-19 05:03:16 +0300

    Update omnilistic.js

--- dist/omnilistic.js
+++ dist/omnilistic.js
@@ -1,10 +1,10 @@
-// Omnilistic  Loader
-import './omnilistic-card.js';
-import './omnilistic-speedtest.js';
-import './minimal-purifier-card.js';
+// Omnilistic Suite Loader
+import '/hacsfiles/omnilistic/omnilistic-card.js';
+import '/hacsfiles/omnilistic/omnilistic-speedtest.js';
+import '/hacsfiles/omnilistic/minimal-purifier-card.js';
 
 console.info(
-  "%c OMNILISTIC %c Loaded Successfully ",
+  "%c OMNILISTIC SUITE %c Loaded Successfully ",
   "color: white; background: #039be5; font-weight: 700;",
   "color: #039be5; background: white; font-weight: 700;"
 );
