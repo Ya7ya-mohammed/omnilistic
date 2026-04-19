546403d620f9006022c04a65034eb077a0fd37fd
Ya7ya-mohammed <suber_wolf@icloud.com>
2026-04-19 04:49:58 +0300

    Create omnilistic.js

---
+++ dist/omnilistic.js
@@ -0,0 +1,10 @@
+// Omnilistic  Loader
+import './omnilistic-card.js';
+import './omnilistic-speedtest.js';
+import './minimal-purifier-card.js';
+
+console.info(
+  "%c OMNILISTIC %c Loaded Successfully ",
+  "color: white; background: #039be5; font-weight: 700;",
+  "color: #039be5; background: white; font-weight: 700;"
+);
